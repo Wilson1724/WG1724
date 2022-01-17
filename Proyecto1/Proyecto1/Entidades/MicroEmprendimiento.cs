80d7defec5a77fe0642ca5d2d7312c0b7474ce76
Wilson1724 <61748173+Wilson1724@users.noreply.github.com>
2022-01-17 15:50:25 -0500

    Add files via upload

---
+++ Proyecto1/Proyecto1/Entidades/MicroEmprendimiento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1.Entidades
+{
+    public class MicroEmprendimiento
+    {
+        public int EmproId { get; set; }
+        public string nombreE { get; set; }
+        public string tipo { get; set; }
+
+        public string desccripcions { get; set; }
+        public string Ciudad { get; set; }
+        public int CreditoNecesario { get; set; }
+        public int Ganancias { get; set; }
+        public int MicroId { get; set; }
+        public Microempresario MicroempresarioEmpr { get; set; }
+    }
+}
