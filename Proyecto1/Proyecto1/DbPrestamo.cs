ce344cfaa47e9be5cc4242971bb0f876ff722150
Wilson1724 <61748173+Wilson1724@users.noreply.github.com>
2022-01-17 15:39:49 -0500

    Add files via upload

---
+++ Proyecto1/Proyecto1/DbPrestamo.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto1.Entidades;
+namespace Proyecto1
+{
+    public class DbPrestamo:DbContext
+    {
+        public DbSet<Prestamo> Prestamos { get; set; }
+        public DbSet<Banco> Bancos { get; set; }
+        public DbSet<Microempresario> Microempresarios { get; set; }
+        public DbSet<MicroEmprendimiento> Microemprendimientos { get; set; }
+        public DbSet<Garante> Garantes { get; set; }
+    }
+}
